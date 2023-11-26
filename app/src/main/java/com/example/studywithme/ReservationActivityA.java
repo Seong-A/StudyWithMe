b1e922d4aa36d44b02fa70ecac7a4c111ab151ca
Seong-A <ksa6777@naver.com>
2023-11-26 18:52:02 +0900

    modify reservation

--- app/src/main/java/com/example/studywithme/ReservationActivityA.java
+++ app/src/main/java/com/example/studywithme/ReservationActivityA.java
@@ -2,6 +2,9 @@ package com.example.studywithme;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,14 +19,21 @@ public class ReservationActivityA extends AppCompatActivity {
         String cafeId = intent.getStringExtra("cafeId");
 
         int layoutResourceId;
+        int totalSeats;
+
+        // 각 카페에 따라 레이아웃 및 총 좌석 수 설정
         if ("cafe1".equals(cafeId)) {
             layoutResourceId = R.layout.activity_reservation_a1;
+            totalSeats = 36;
         } else if ("cafe2".equals(cafeId)) {
             layoutResourceId = R.layout.activity_reservation_b1;
+            totalSeats = 40;
         } else if ("cafe3".equals(cafeId)) {
             layoutResourceId = R.layout.activity_reservation_c1;
+            totalSeats = 46;
         } else if ("cafe4".equals(cafeId)) {
             layoutResourceId = R.layout.activity_reservation_d1;
+            totalSeats = 44;
         } else {
             return;
         }
@@ -32,5 +42,28 @@ public class ReservationActivityA extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        // 좌석 개수에 맞게 반복문을 통해 클릭 리스너 등록
+        for (int i = 1; i <= totalSeats; i++) {
+            setSeatClickListener(i);
+        }
+    }
+
+    private void setSeatClickListener(int seatNum) {
+        // 리소스 ID를 가져오기 위해 좌석 ID 설정
+        String seatIdName = "seat" + seatNum;
+        int seatId = getResources().getIdentifier(seatIdName, "id", getPackageName());
+
+        ImageView seatImageView = findViewById(seatId);
+
+        seatImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show reservation button or perform other actions
+                View reservationButton = findViewById(R.id.reservation_btn);
+                reservationButton.setVisibility(View.VISIBLE);
+                Toast.makeText(ReservationActivityA.this, "좌석 " + seatNum + "을 선택하였습니다", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
